f3ec238687099824708bf7fae541b73bb32329e0
Nekori228 <88834448+Nekori228@users.noreply.github.com>
2026-09-09 22:39:19 +0500

    fillArray массив

--- 01.cpp
+++ 01.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
-
-void fillArray() {
-    
-}
-
-void printArray() {
-    
-}
-
-void swapElements() {
-    
-}
-
-void multiplyByTwo() {
-    
-}
+#include <cstdlib>
+#include <ctime>
 
 int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+    
+    int myArray[10];
+    
+    for (int i = 0; i < 10; ++i) {
+        myArray[i] = rand() % 100 + 1;
+        std::cout << myArray[i] << " ";
+    }
+    std::cout << std::endl;
     
     return 0;
 }
