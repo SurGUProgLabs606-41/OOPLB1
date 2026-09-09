289c96f230aca2b287acf2d3c6cfadba7a7f1279
Nekori228 <88834448+Nekori228@users.noreply.github.com>
2026-09-09 22:30:45 +0500

    Начало выполнения первого задания

---
+++ 01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void fillArray() {
+    
+}
+
+void printArray() {
+    
+}
+
+void swapElements() {
+    
+}
+
+void multiplyByTwo() {
+    
+}
+
+int main() {
+    
+    return 0;
+}
