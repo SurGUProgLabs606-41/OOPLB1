cee613ab09bae0a62efd181b778a6510ca881d20
Nekori228 <88834448+Nekori228@users.noreply.github.com>
2026-09-09 22:53:12 +0500

    Вынос заполнения в отдельную функцию

--- 01.cpp
+++ 01.cpp
@@ -2,13 +2,21 @@
 #include <cstdlib>
 #include <ctime>
 
+// Функция заполнения массива
+void fillArray(int (&arr)[10]) {
+    for (int i = 0; i < 10; ++i) {
+        arr[i] = rand() % 100 + 1;
+    }
+}
+
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
     
     int myArray[10];
     
+    fillArray(myArray);
+    
     for (int i = 0; i < 10; ++i) {
-        myArray[i] = rand() % 100 + 1;
         std::cout << myArray[i] << " ";
     }
     std::cout << std::endl;
